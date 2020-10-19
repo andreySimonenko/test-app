6c27f44100e35821a471a07669dd7cc195488e3b
Andrey Simonenko <a.simonenko@kt-team.de>
2020-10-19 15:58:33 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,8 @@ const NODE_ENV = process.env.NODE_ENV === 'development'
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  publicPath: NODE_ENV === 'production'
+    ? '/test-app/'
+    : '/'
 }
